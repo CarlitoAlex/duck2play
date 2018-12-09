5d033ca1476b8aeb115c012dc724e891a6a19ecb
carlitoAlex716 <alexurchenko91@gmail.com>
2018-12-09 03:24:53 +0200

    Delete test

--- src/test/java/com/duck2play/duck2play/Duck2playApplicationTests.java
+++ src/test/java/com/duck2play/duck2play/Duck2playApplicationTests.java
@@ -9,32 +9,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Duck2playApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-	@Test
-	public void test1(){
-		int a = 123;
-	}
-
-	@Test
-	public void test2(){
-		int a = 1;
-		int b = a + 5;
-		Assert.assertEquals(2,b);
-	}
-
-	@Test
-	public void testSeven(){
-		String seven = "seven";
-		if (seven.length()==5){
-			System.out.println("Seven");
-		}else {
-			System.out.println("So Sad");
-		}
-	}
-
-
+	
 }
